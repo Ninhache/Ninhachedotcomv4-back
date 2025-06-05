131a26e7d93518bb5d7db8592622146b5d72c297
Ninhache <mr.carambar27@gmail.com>
2025-06-05 18:59:48 +0200

    chore: init many things

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3000;
+  
+
+  console.log(`Waiting on port http://localhost:${port}`)
+  await app.listen(port);
+}
+bootstrap();
